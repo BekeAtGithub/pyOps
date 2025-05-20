c8163da19302514cba8387989a87550d49163e06
Adam Marton Beke <65732867+BekeAtGithub@users.noreply.github.com>
2025-05-20 06:33:30 -0600

    Update pipelineMonitor.py

--- pipeline/pipelineMonitor.py
+++ pipeline/pipelineMonitor.py
@@ -1,4 +1,4 @@
-# Importing the required modules
+# Importing the required modules 
 import subprocess  # subprocess is used to run shell commands
 import requests  # requests is a library for making HTTP requests 
 import time  # time is used for pausing execution and timing operations  
