46521242fb06fbc886a9a0ecd4aaccd80e3bf4e1
Adam Marton Beke <65732867+BekeAtGithub@users.noreply.github.com>
2025-11-19 15:14:52 -0700

    Update helmTalk.py

--- pipeline/helmTalk.py
+++ pipeline/helmTalk.py
@@ -1,4 +1,4 @@
-#uses Helm CLI via python to manage helm charts.  
+#uses Helm CLI via python to manage helm charts
 #helm doesnt have an API so python executes helm commands.
 #Python uses subprocess module to run commands like;
 #helm list -all-namespaces   
